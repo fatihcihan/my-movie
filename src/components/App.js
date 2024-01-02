2c95b0e64a2d2ebf1349633f8c0025a35d8e22a5
Fatih <fatihcihan666@gmail.com>
2024-01-02 23:18:29 +0300

    deleteMovie function was written and passed to the MovieList component via props

--- src/components/App.js
+++ src/components/App.js
@@ -32,6 +32,15 @@ class App extends React.Component {
     ]
   }
 
+  deleteMovie = (movie) => {
+    const newMovieList = this.state.movies.filter(
+      m => m.id !== movie.id
+    );
+
+    this.setState({
+      movies: newMovieList
+    });
+  }
 
   render() {
     return (
@@ -42,7 +51,8 @@ class App extends React.Component {
           </div>
         </div>
         <MovieList
-          movies={this.state.movies} />
+          movies={this.state.movies}
+          deleteMovieProp={this.deleteMovie} />
       </div>
     );
   }
