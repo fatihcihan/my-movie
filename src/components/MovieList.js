bdc0e110b42363dcd0766e87ea0010102c59e5db
Fatih <fatihcihan666@gmail.com>
2024-01-02 19:53:53 +0300

    received data from App.js via props and made it dynamic using the map function

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,19 +4,21 @@ class MovieList extends React.Component {
     render() {
         return (
             <div className="row">
-                <div className="col-lg-4">
-                    <div className="card mb-4 shadow-sm">
-                        <img src="https://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg" className="card-img-top" alt="" />
-                        <div className="card-body">
-                            <h5 className="card-title">Sample Movie</h5>
-                            <p className="card-text">Sample Movie Description</p>
-                            <div className="d-flex justify-content-between align-items-center">
-                                <button type="button" className="btn btn-md btn-outline-danger">Delete</button>
-                                <h2><span className="badge bg-secondary">9.0</span></h2>
+                {this.props.movies.map((movie) => (
+                    <div className="col-lg-4">
+                        <div className="card mb-4 shadow-sm">
+                            <img src={movie.imageURL} className="card-img-top" alt="" />
+                            <div className="card-body">
+                                <h5 className="card-title">{movie.name}</h5>
+                                <p className="card-text">{movie.overview}</p>
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <button type="button" className="btn btn-md btn-outline-danger">Delete</button>
+                                    <h2><span className="badge bg-secondary">{movie.rating}</span></h2>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                ))}
             </div>
         );
     }
