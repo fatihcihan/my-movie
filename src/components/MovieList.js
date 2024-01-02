0e7e62723cba1140d183051dd527f6f1f26eacb8
Fatih <fatihcihan666@gmail.com>
2024-01-02 18:59:07 +0300

    class based component created

---
+++ src/components/MovieList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MovieList extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Movie List</h1>
+            </div>
+        );
+    }
+}
+
+
+export default MovieList; 
