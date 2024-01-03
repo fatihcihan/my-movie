5082b55e1ed1eaccdcfa96eadab2fdfe6ba638bc
Fatih <fatihcihan666@gmail.com>
2024-01-03 14:01:26 +0300

    moved state to App.js and passed searchMovieProp from App.js as props for onChange

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,24 +2,19 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
-    state = {
-        searchQuery: ""
-    }
-
     handleFormSubmit = (event) => {
         event.preventDefault();     // we stopped the default behavior
     }
 
     render() {
-        console.log(typeof handleFormSubmit);
         return (
             <form onSubmit={this.handleFormSubmit}>
                 <div className='form-row mb-5'>
                     <div className='col-12'>
                         <input
-                            onChange={(event) => this.setState({ searchQuery: event.target.value })}
+                            onChange={this.props.searchMovieProp}
                             type="text" className="form-control" placeholder="Search a movie"
-                            value={this.state.searchQuery} />
+                        />
                     </div>
                 </div>
             </form>
