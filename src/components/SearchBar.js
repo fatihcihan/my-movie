f87f190d9166155eddafd6e26af52124999f10ef
Fatih <fatihcihan666@gmail.com>
2024-01-05 15:21:22 +0300

    added add movie button and updated bootstrap classes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,12 +9,16 @@ class SearchBar extends React.Component {
     render() {
         return (
             <form onSubmit={this.handleFormSubmit}>
-                <div className='form-row mb-5'>
-                    <div className='col-12'>
+                <div className='row mb-5'>
+                    <div className='col-9'>
                         <input
                             onChange={this.props.searchMovieProp}
                             type="text" className="form-control" placeholder="Search a movie"
                         />
+                    </div>
+                    <div className='col-3'>
+                        <button type="button"
+                            className="btn btn-danger" style={{ float: 'right' }}>Add Movie</button>
                     </div>
                 </div>
             </form>
