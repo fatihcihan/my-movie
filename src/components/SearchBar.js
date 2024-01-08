fc07b0d86a6c51099365240b0d3911764252b2d5
Fatih <fatihcihan666@gmail.com>
2024-01-08 23:48:54 +0300

    Link imported, changed to button link and redirected

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class SearchBar extends React.Component {
 
@@ -17,8 +18,9 @@ class SearchBar extends React.Component {
                         />
                     </div>
                     <div className='col-3'>
-                        <button type="button"
-                            className="btn btn-danger" style={{ float: 'right' }}>Add Movie</button>
+                        <Link to="/add"
+                            type="button"
+                            className="btn btn-danger" style={{ float: 'right' }}>Add Movie</Link>
                     </div>
                 </div>
             </form>
