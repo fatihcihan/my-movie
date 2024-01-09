20ec6db28d44a5dbed9e3fbc04ce6e10c8899201
Fatih <fatihcihan666@gmail.com>
2024-01-09 05:04:02 +0300

    converted class to component, created state and imported axios

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -1,57 +1,71 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-const EditMovie = (props) => {
-    const handleFormSubmit = (e) => {
-        e.preventDefault();       
-        navigate('/');
+class EditMovie extends React.Component {
+
+    state = {
+        name: "",
+        rating: "",
+        overview: "",
+        imageUrl: ""
     }
 
-    console.log(props, 'props');
-    const navigate = useNavigate();
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+        // navigate('/');
+    }
 
-    return (
-        <div className="container">
-            <form className="mt-5" onSubmit={handleFormSubmit}>
-                <input className="form-control" id="disabledInput" type="text" placeholder="Edit The Form To Update A Movie.." disabled />
-                <div className="form-row">
-                    <div className="form-group col-md-10">
-                        <label htmlFor="inputName">Name</label>
-                        <input type="text"
-                            className="form-control"
-                            name="name" />
+    async componentDidMount() {
+        // await axios.get(`http://localhost:3002/movies/${id}`);
+    }
+
+    render() {
+        console.log(this.state);
+        return (
+
+            <div className="container" >
+                <form className="mt-5" onSubmit={this.handleFormSubmit}>
+                    <input className="form-control" id="disabledInput" type="text" placeholder="Edit The Form To Update A Movie.." disabled />
+                    <div className="form-row">
+                        <div className="form-group col-md-10">
+                            <label htmlFor="inputName">Name</label>
+                            <input type="text"
+                                className="form-control"
+                                name="name" />
+                        </div>
+                        <div className="form-group col-md-2">
+                            <label htmlFor="inputRating">Rating</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                name="rating" />
+                        </div>
                     </div>
-                    <div className="form-group col-md-2">
-                        <label htmlFor="inputRating">Rating</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            name="rating" />
+                    <div className="form-row">
+                        <div className="form-group col-md-12">
+                            <label htmlFor="inputImage">Image URL</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                name="imageURL" />
+                        </div>
                     </div>
-                </div>
-                <div className="form-row">
-                    <div className="form-group col-md-12">
-                        <label htmlFor="inputImage">Image URL</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            name="imageURL" />
+                    <div className="form-row">
+                        <div className="form-group col-md-12">
+                            <label htmlFor="overviewTextarea">Overview</label>
+                            <textarea
+                                className="form-control"
+                                name="overview" rows="5"></textarea>
+                        </div>
                     </div>
-                </div>
-                <div className="form-row">
-                    <div className="form-group col-md-12">
-                        <label htmlFor="overviewTextarea">Overview</label>
-                        <textarea
-                            className="form-control"
-                            name="overview" rows="5"></textarea>
-                    </div>
-                </div>
-                <input type="submit"
-                    className="btn btn-danger btn-block"
-                    value="Add Movie" />
-            </form>
-        </div>
-    );
+                    <input type="submit"
+                        className="btn btn-danger btn-block"
+                        value="Add Movie" />
+                </form>
+            </div >
+        );
+    }
 }
 
 
