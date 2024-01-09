1b59b4473d2d0ce673695541bf73c404c100769d
Fatih <fatihcihan666@gmail.com>
2024-01-09 15:20:05 +0300

    converted to functional based component. useState, useParams and useEffect hooks were used

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -1,71 +1,100 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 
-class EditMovie extends React.Component {
+const EditMovie = () => {
 
-    state = {
+    /*   state = {
+          name: "",
+          rating: "",
+          overview: "",
+          imageUrl: ""
+      } */
+
+    const [movie, setMovie] = useState({
         name: "",
         rating: "",
         overview: "",
-        imageUrl: ""
-    }
+        imageURL: ""
+    });
 
-    handleFormSubmit = (e) => {
-        e.preventDefault();
-        // navigate('/');
-    }
+    console.log(movie);
 
-    async componentDidMount() {
-        // await axios.get(`http://localhost:3002/movies/${id}`);
-    }
+    const { id } = useParams();
+    console.log(id);
 
-    render() {
-        console.log(this.state);
-        return (
+    useEffect(() => {
+        const getMovie = async () => {
+            const response = await axios.get(`http://localhost:3002/movies/${id}`);
+            console.log(response, 'res');
+            const movie = response.data;
+            console.log(movie, 'movie');
 
-            <div className="container" >
-                <form className="mt-5" onSubmit={this.handleFormSubmit}>
-                    <input className="form-control" id="disabledInput" type="text" placeholder="Edit The Form To Update A Movie.." disabled />
-                    <div className="form-row">
-                        <div className="form-group col-md-10">
-                            <label htmlFor="inputName">Name</label>
-                            <input type="text"
-                                className="form-control"
-                                name="name" />
-                        </div>
-                        <div className="form-group col-md-2">
-                            <label htmlFor="inputRating">Rating</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                name="rating" />
-                        </div>
+            setMovie({
+                name: movie.name,
+                rating: movie.rating,
+                overview: movie.overview,
+                imageURL: movie.imageURL,
+            })
+        };
+        getMovie();
+    }, [id])
+
+    // onSubmit={this.handleFormSubmit}
+
+    /*  handleFormSubmit = (e) => {
+         e.preventDefault();
+         // navigate('/');
+     } */
+
+
+    return (
+
+        <div className="container" >
+            <form className="mt-5">
+                <input className="form-control" id="disabledInput" type="text" placeholder="Edit The Form To Update A Movie.." disabled />
+                <div className="form-row">
+                    <div className="form-group col-md-10">
+                        <label htmlFor="inputName">Name</label>
+                        <input type="text"
+                            className="form-control"
+                            name="name"
+                            value={movie.name} />
                     </div>
-                    <div className="form-row">
-                        <div className="form-group col-md-12">
-                            <label htmlFor="inputImage">Image URL</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                name="imageURL" />
-                        </div>
+                    <div className="form-group col-md-2">
+                        <label htmlFor="inputRating">Rating</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="rating"
+                            value={movie.rating} />
                     </div>
-                    <div className="form-row">
-                        <div className="form-group col-md-12">
-                            <label htmlFor="overviewTextarea">Overview</label>
-                            <textarea
-                                className="form-control"
-                                name="overview" rows="5"></textarea>
-                        </div>
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-12">
+                        <label htmlFor="inputImage">Image URL</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="imageURL"
+                            value={movie.imageURL} />
                     </div>
-                    <input type="submit"
-                        className="btn btn-danger btn-block"
-                        value="Add Movie" />
-                </form>
-            </div >
-        );
-    }
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-12">
+                        <label htmlFor="overviewTextarea">Overview</label>
+                        <textarea
+                            className="form-control"
+                            name="overview" rows="5"
+                            value={movie.overview}></textarea>
+                    </div>
+                </div>
+                <input type="submit"
+                    className="btn btn-danger btn-block"
+                    value="Add Movie" />
+            </form>
+        </div >
+    );
 }
 
 
